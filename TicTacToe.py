cb843d070beed7935d8241c8dbce2eaf41859e39
Dany Zaky <danyzakydany123@gmail.com>
2020-12-06 19:59:58 +0700

    add oop template

--- TicTacToe.py
+++ TicTacToe.py
@@ -1,1 +1,68 @@
-print("raupenak")
+class MainMenu:
+    def __init__ (self, playGame, selectCharacter, selectMode, selectMap, exitGame):
+        self.playGame = playGame
+        self.selectCharacter = selectCharacter
+        self.selectMode = selectMode
+        self.selectMap = selectMap
+        self.exitGame = exitGame
+
+    def set_character():
+        pass
+
+    def get_character():
+        pass
+
+    def set_mode():
+        pass
+
+    def get_mode():
+        pass
+
+    def set_map():
+        pass
+
+    def get_map():
+        pass
+
+
+class Play_Game:
+    def start_game():
+        pass
+
+
+class Select_Character:
+    def __init__ (self, character):
+        self.character = character
+
+    def selecting_character():
+        pass
+
+    def selected_character():
+        pass
+
+
+class selectMap:
+    def __init__ (self, map):
+        self.map = map
+
+    def selecting_map():
+        pass
+
+    def selected_map():
+        pass
+
+
+class selectMode:
+    def __init__ (self, mode):
+        self.mode = mode
+
+    def selecting_mode():
+        pass
+
+    def selected_mode():
+        pass
+
+
+class exitGame:
+    def end_game():
+        pass
